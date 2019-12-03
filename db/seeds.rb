ebacd210210e89137c6057677c2056d065bb6685
github username <github email address>
2019-12-03 14:57:32 -0600

    added .each methods to the seed data, STILL DID NOT PUSH, needs to be reviewed and fixed before seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,9 @@ battles_list = [
     [1, 2, NULL, 1],
     [1, 3, NULL, 1]
 ]
+battles_list.each do |player_id, npc_id, player_win, match_id|
+    Battle.create(player_id:, player_id, npc_id: npc_id, player_win: player_win, match_id: match_id)
+end
 
 # battle_1, battle_2, battle_3, battle_4, battle_5
 matches_list = [
@@ -11,6 +14,9 @@ matches_list = [
     [NULL, NULL, NULL, NULL, NULL],
     [NULL, NULL, NULL, NULL, NULL]
 ] # Maybe add an id to match the boss/villain at that level per match (set of battles)
+matches_list.each do |battle_1, battle_2, battle_3, battle_4, battle_5|
+    Match.create(battle_1: battle_1, battle_2: battle_2, battle_3: battle_3, battle_4: battle_4, battle_5: battle_5)
+end
 
 # Npc.create(name: "blop", species: "unshaped", npc_species_id: 1 )
 # name, species, npc_species_id
@@ -19,6 +25,9 @@ npc_list = [
     ["Mrs. Karen", "Teacher", 2]
     ["'Mom'", "Evil Step-Mother", 3]
 ]
+npc_list.each do |name, species, npc_species_id|
+    Npc.create(name: name, species: species, npc_species_id: npc_species_id)
+end
 
 # Npc_specie.create(hp: 10, iq: , str:, lk:, npc_attack:, npc_defense: , ult:  )
 #hp, iq, str, lk, npc_attack, npc_defense, ult
@@ -27,23 +36,25 @@ npc_species_list = [
     [10, 10, 4, 2, "Ruler Smack", "'Well I never'", "Detention!"],
     [12, 6, 5, 8, "Chancla!", "'Because I said so!'", "Bring me your manager!"]
 ] #[24, 26, 34]
+npc_species_list.each do |hp, iq, str, lk, npc_attack, npc_defense, ult|
+    NpcSpecie.create(hp: hp, iq: iq, str: str, lk: lk, npc_attack: npc_attack, npc_defense: npc_defense, ult: ult)
+end
 
 # attack, defense, wildcard, player_id
 player_moves_list = [
     ["Lab Expiriment", "Fetal Position", "I'm rubber, and you're glue!"]
 ]
+player_moves_list.each do |attack, defense, wildcard, player_id|
+    PlayerMove.create(attack: attack, defense: defense, wildcard: wildcard, player_id: player_id)
+end
 
 # name, hp, iq, str, lk
 player_list = [
     [NULL, 8, 9, 4, 3]
 ] #[24] + 8 = [32] (plus one per boss defeated) Consider adding species column that says NERD
-
-
-
-
-
-
-
+player_list.each do |name, hp, iq, str, lk|
+    Player.create(name: name, hp: hp, iq: iq, str: str, lk: lk)
+end
 
 
 # country_list = [
